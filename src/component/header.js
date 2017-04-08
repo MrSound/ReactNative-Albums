2bedbbf72cb367b77c7aec47e8c5e13efc2a9915
chaithep <sound014@gmail.com>
2017-04-08 22:00:12 +0700

    Display with Style

--- src/component/header.js
+++ src/component/header.js
@@ -1,11 +1,22 @@
 // 1. Import library for making component
 import React from 'react';
-import { Text } from 'react-native';
-
+import { Text, View } from 'react-native';
 // 2. Make a component
-const Header = () => (
-    <Text>Albums</Text>
-);
-
+const Header = () => {
+    const { viewStyle, textStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    );
+};
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
 // 3. Make the component available to other part of the app
 export default Header;
