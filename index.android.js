c382a9a2dfddef541e048d5a98498bb4c6977ab6
MrSound <sound014@gmail.com>
2017-04-10 01:37:00 +0700

    Create AlbumList

--- index.android.js
+++ index.android.js
@@ -1,13 +1,16 @@
 // Index.android.js - place code in here for Android!!!!
-
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/component/header';
+import AlbumList from './src/component/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
--- src/component/header.js
+++ src/component/header.js
@@ -1,21 +1,36 @@
 // 1. Import library for making component
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 // 2. Make a component
-const Header = () => {
+const Header = (props) => {
     const { viewStyle, textStyle } = styles;
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Albums</Text>
+            <Text style={textStyle}>{props.headerText}</Text>
         </View>
     );
 };
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8'
+        backgroundColor: '#F8F8F8', //F8F8F8
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        ...Platform.select({
+            ios: {
+                shadowColor: '#ff0000',
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.2,
+            },
+            android: {
+                elevation: 2,
+                position: 'relative'
+            }
+        }),
     },
     textStyle: {
-        fontSize: 20
+        fontSize: 20,
+        color: '#000'
     }
 };
 // 3. Make the component available to other part of the app
