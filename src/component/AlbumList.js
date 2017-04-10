b13d3563d8039ef62e0027ad37ddb38ca045e362
MrSound <sound014@gmail.com>
2017-04-11 01:26:53 +0700

    axios get API data

--- src/component/AlbumList.js
+++ src/component/AlbumList.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends React.Component {
+    componentWillMount() {
+        // fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        //     //.then((response) => response.json())
+        //     //.then((responseData) => this.setState({ albums: responseData }));
+        //     .then((response) => { console.log('Response 1 :', response); return response.json(); })
+        //     .then((responseData) => console.log('Response 2 :', responseData));
+        // //debugger;
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log('Response 3 :', response));
+    }
+
     render() {
         return (
             <View>
