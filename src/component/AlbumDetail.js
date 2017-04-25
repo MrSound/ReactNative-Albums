f4a5610c04edbefa88a66dd50fa89638e5fc2d32
MrSound <sound014@gmail.com>
2017-04-26 01:47:08 +0700

    create Card.js,CardSection.js

--- src/component/AlbumDetail.js
+++ src/component/AlbumDetail.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
+// AlbumDetail.js
 const AlbumDetail = (props) => (
-    <View>
-        <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+        <CardSection>
+            <Text>{props.album.title}</Text>
+        </CardSection>
+    </Card>
 );
 
 export default AlbumDetail;
